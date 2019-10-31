ec9d5aea819f1b360c39d9cea3bbde0bd5fad7f9
ajeyodey <ajeyo.dey@gmail.com>
2019-10-31 18:44:10 +0530

    more changess

--- search/models.py
+++ search/models.py
@@ -20,6 +20,6 @@ class Post(models.Model):
     #     self.published_date = timezone.now()
     #     self.save()
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
